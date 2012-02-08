c3be538abbaed9ed68bc2060d0ad6dae1be5c867
Mark Chapman <mark.chapman@gmail.com>
2012-02-08 16:44:41 +0000

    Correct port

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.post('/wish/new', function(req, res){
 });
 
 
-app.listen(3000);
+app.listen(env.PORT);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
