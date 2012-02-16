d7a4427c3785294a6664c339b2733fde53babe1d
Mark Chapman <mark.chapman@gmail.com>
2012-02-16 15:50:46 +0000

    Get rid of slight change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.set('jsonp callback');
+  app.enable('jsonp callback');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
